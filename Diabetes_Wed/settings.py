2a9a9142ac2965b6d78d4f5cb14bb83e7e9683ed
Tzu-Yang-C <B10327038@gmail.com>
2025-06-05 16:21:44 +0800

    4

--- Diabetes_Wed/settings.py
+++ Diabetes_Wed/settings.py
@@ -125,10 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),  # 告訴 Django 要去哪裡找靜態檔案
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'staticfiles'),  # 告訴 Django 要去哪裡找靜態檔案
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
